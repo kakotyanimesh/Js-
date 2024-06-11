59dd6a6281375dd0fcf8a3f178edcc0f3f46adf0
animesh <phb21013@tezu.ac.in>
2024-06-11 16:57:40 +0530

    bst

--- binarySearchtree/practice.js
+++ binarySearchtree/practice.js
@@ -36,4 +36,40 @@ class binarySearchTree{
             }
         }
     }
+
+    delete(key){
+        this.root = this.deleteNode(this.root, key)
+    }
+
+    deleteNode(node, key){
+        if(node.key === null){
+            return null
+        }
+
+        if(key < node.key){
+            node.left = this.deleteNode(node.left, key)
+        } else if (key > node.key) {
+            node.right = this.deleteNode(node.right, key)
+        } else {
+            if(node.left === null && node.right === null){
+                return null
+            } else if(node.left === null){
+                return node.right
+            } else if (node.right === null) {
+                return node.left
+            } else {
+                let tempNode = this.findMin(node.right)
+                node.key = tempNode.key
+                node.right = this.deleteNode(node.right, tempNode.key)
+            }
+        }
+        return node
+    }
+
+    findMin(node){
+        while(node.left === null){
+            node = node.left
+        }
+        return node
+    }
 }
