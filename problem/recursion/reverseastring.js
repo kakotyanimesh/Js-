182052483665151abef26b928d79a099fc190d5f
animesh <phb21013@tezu.ac.in>
2024-06-14 16:49:16 +0530

    Update reverseastring.js

--- problem/recursion/reverseastring.js
+++ problem/recursion/reverseastring.js
@@ -16,14 +16,43 @@
 
 
 
+let recursivereversestring = (string) => {
+    if(string === '') return string
 
-function recursiveastring(string){
-    if(string === "") return string
-
-    let reverse = recursiveastring(string.substring(0, string.length - 1))
+    let reverse = recursivereversestring(string.substring(0, string.length - 1))
     let result = string[string.length -1] + reverse
+
+    console.log(`Reversing: Last char: ${string[string.length- 1]} added in front of ${reverse} to form a result: ${result}`)
+    return result
+}
+
+console.log(recursivereversestring("animesh"));
+
+
+
+let ittreateREVSERSE = (string) => {
+    let reverse = ""
+    let length = string.length - 1
+
+    while (length >= 0) {
+        reverse += string[length]
+        length = length -1
+    }
+    return reverse
+}
+
+console.log(ittreateREVSERSE("kakoty"));
+
+
+function recursive(string) {
+    if (string === "") {
+        return string
+    }
+
+    let reverse = recursive(string.substring(0, string.length-1))
+    let result = string[string.length - 1] + reverse
 
     return result
 }
 
-console.log(recursiveastring("animesh"));
+console.log(recursive("popopop"));
