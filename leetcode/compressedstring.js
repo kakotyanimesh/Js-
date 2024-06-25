11409dfb9742281b3c3499fd1bbc02acfa009bc8
animesh <phb21013@tezu.ac.in>
2024-06-25 23:11:41 +0530

    11:11

---
+++ leetcode/compressedstring.js
@@ -0,0 +1,29 @@
+
+var compress = function(chars) {
+    let index = 0
+    let i = 0 
+
+    while (i < chars.length) {
+        let j = i 
+
+        while (j < chars.length && chars[i] === chars[j]) {
+            j++
+        }
+
+        let count = j - i 
+
+        chars[index++] = chars[i]
+
+        if (count > 1) {
+            for (const str of count.toString()) {
+                chars[index++] = str
+            }
+        }
+        i = j 
+    }
+    return index
+};
+
+
+console.log(compress(["a","a","b","b","c","c","c"]));
+
