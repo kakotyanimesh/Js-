df9a7268a991c8f9ddc5261faa5f66e4435d7877
animesh <phb21013@tezu.ac.in>
2024-11-07 23:42:29 +0530

    practiced stack and queue along with this learnt about the shift , unshift and join , shift removes the 0th index's element from array and ushift add element at th 0th Index and join can seperate elements from array and make then a string with seperator which is inside curle braces

--- InterviewPractice/dayOne.js
+++ InterviewPractice/dayOne.js
@@ -100,6 +100,92 @@ console.log(myStack.isEmpty());
 console.log(myStack);
 
 
+
+
+// Queue  => same as the stack thing but it follows the first in First out method. means if we added data first then it will execute in the first, usages in e commerce sites for ordering things or in Os cpu might use the queue to do tasks. 
+
+
+class Queue{
+    constructor(){
+        this.queue = []
+    }
+
+    // add things but in last => we call it enqueue
+    enqueue(e){
+        this.queue.push(e)
+    }
+
+    //  remove item but that one who added in the first => we call it dequeue and use shift() => returns the shifted element and shifted the prev e to its next position
+    dequeue(){
+        if(this.isEmpty()){
+            return 'Queue is Empty'
+        }
+        return this.queue.shift()
+    }
+
+    isEmpty(){
+        return this.queue.length === 0
+    }
+
+    // peek is to return the top most element and here 0th index's element is the top element 
+    peek(){
+        return this.queue[0]
+    }
+
+    size(){
+        return this.queue.length
+    }
+
+    clear(){
+        this.queue = []
+    }
+
+    print(){
+        let str = ''
+        for(let i = 0; i< this.queue.length; i++){
+            str += this.queue[i] + "\n"
+        }
+        return str
+    }
+
+    printInJoin(){
+        return this.queue.join(" , ")
+    }
+
+}
+
+
+
+const myQueue = new Queue()
+myQueue.enqueue(12)
+myQueue.enqueue(66)
+myQueue.enqueue(55)
+myQueue.enqueue(145)
+console.log(myQueue.print());
+myQueue.dequeue()
+console.log(myQueue.print());
+console.log(myQueue.printInJoin());
+console.log(myQueue.isEmpty());
+console.log(myQueue.peek());
+console.log(myQueue.size());
+myQueue.enqueue(12)
+myQueue.enqueue(66)
+myQueue.enqueue(55)
+myQueue.enqueue(145)
+console.log(myQueue.printInJoin());
+myQueue.clear()
+console.log(myQueue.isEmpty());
+console.log(myQueue.printInJoin());
+
+
+
+
+
+
+
+
+
+
 // class newStack{
 //     constructor(){
 //         this.stack = []
@@ -159,3 +245,50 @@ console.log(myStack);
 // myStack.push(7)
 // console.log(myStack.print());
 
+
+
+// class Queue{
+//     constructor(){
+//         this.queue = [] 
+//     }
+
+//     enqueue(e){
+//         this.queue.push(e)
+//     }
+
+//     dequeue(){
+//         this.queue.shift()
+//     }
+
+//     isEmpty(){
+//         return this.queue.length === 0
+//     }
+
+//     clear(){
+//         this.queue = []
+//     }
+
+//     peek(){
+//         return this.queue[0]
+//     }
+
+//     size(){
+//         return this.queue.length
+//     }
+
+//     includes(e){
+//         return this.queue.includes(e)
+//     }
+
+//     print(){
+//         let str = ""
+//         for(let i =0; i< this.queue.length; i++){
+//             str += this.queue[i]
+//         }
+//         return str
+//     }
+
+//     printInJoin(){
+//         return this.queue.join(" , ")
+//     }
+// }
